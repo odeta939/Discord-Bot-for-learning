63d4d179c6c851f4d57e8706789f9e1f7e658e7d
odeta <odeta@disputas.no>
2021-04-04 17:53:21 +0200

    the bot asks random question, provides possible answers, waits for answer and tells if the answer was correct

--- app.js
+++ app.js
@@ -8,17 +8,50 @@ client.login(process.env.BOT_TOKEN)
 client.on('ready', ()=> {
     console.log('Bot is ready');
 });
+let activeQuestion = null;
 
 'ues strict';
 let questions = require('./questions.json');
-console.log(questions);
+console.log(questions[0].question);
 
 
 
-client.on('message', (msg)=>{
-    if (msg.content === '?IoT'){
-        msg.channel.send(questions[0][Math.floor(Math.random() * Object.keys(questions[0]).length)]);
-    }else if(msg.content === "?OS"){
-        msg.channel.send(questions[1][Math.floor(Math.random() * Object.keys(questions[1]).length)]);
+/*client.on('message', (msg)=>{
+  //  if(msg.author == client.user){
+        let randomIndex = Math.floor(Math.random() * Object.keys(questions).length);
+
+        if (msg.content === '?question'){
+            activeQuestion = questions[randomIndex];
+            msg.channel.send(activeQuestion.question);
+            msg.channel.send(activeQuestion.answers);
+        
+        }else if (activeQuestion != null){
+            if(msg.author == client.user && msg.content === activeQuestion.rightAnswer){
+                msg.channel.send("Answer is correct!");
+            }else{
+                msg.channel.send("Correct answer is: " +activeQuestion.rightAnswer)
+        }
+        activeQuestion = null;
     }
 });
+*/
+client.on('message', (msg)=>{
+    if(msg.author == client.user){
+        return
+    }
+    let randomIndex = Math.floor(Math.random() * Object.keys(questions).length);
+    if (msg.content === '?question'){
+        activeQuestion = questions[randomIndex];
+        msg.channel.send(activeQuestion.question);
+        msg.channel.send(activeQuestion.answers);
+    
+    }else if (activeQuestion != null){
+        if(msg.content == activeQuestion.rightanswer){
+            msg.channel.send("Answer is correct!");
+        }else{
+            msg.channel.send("Correct answer is: " +activeQuestion.rightanswer)
+    }
+    activeQuestion = null;
+  }
+
+})

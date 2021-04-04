1ab6d34ceaf152e36c48bf90b2b23ea8b9aba812
odeta <odeta@disputas.no>
2021-04-04 13:35:19 +0200

    commiting all changes

--- app.js
+++ app.js
@@ -9,13 +9,16 @@ client.on('ready', ()=> {
     console.log('Bot is ready');
 });
 
+'ues strict';
+let questions = require('./questions.json');
+console.log(questions);
 
-const questions = [
-    'question one', 'question two', 'question three'
-];
+
 
 client.on('message', (msg)=>{
-    if (msg.content === '?questions'){
-        msg.channel.send(questions[Math.floor(Math.random() * questions.length)]);
+    if (msg.content === '?IoT'){
+        msg.channel.send(questions[0][Math.floor(Math.random() * Object.keys(questions[0]).length)]);
+    }else if(msg.content === "?OS"){
+        msg.channel.send(questions[1][Math.floor(Math.random() * Object.keys(questions[1]).length)]);
     }
 });
